736aafabaef9fcb8962feb63d1dd4c8144487ce1
CAPACITACION-10 <vivelab@ufpso.edu.co>
2019-12-03 16:31:52 -0500

    ultimo

--- app/src/main/java/com/example/organizador/MainActivity.java
+++ app/src/main/java/com/example/organizador/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.organizador;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,12 +9,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.organizador.models.persona;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -71,7 +75,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.ingresar:
 //                Intent intent = new Intent(v.getContext(), dashboard.class);
 //                startActivityForResult(intent,0);
-                auth();
+                if (user.equals("") || pass.equals("")) {
+
+                    validate();
+                }else {
+                    auth();
+                }
                 break;
             case R.id.registrar:
                 Intent intent2 = new Intent(v.getContext(), registro.class);
@@ -79,29 +88,73 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.registrarBD1:
-               register();
+                if (user.equals("") || pass.equals("")) {
+
+                    validate();
+                }else {
+                    register();;
+                }
                 break;
         }
     }
 
     private void auth() {
+        final String user = usuario.getText().toString();
+        String pass = contraseña.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(user, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    int pos = user.indexOf("@"); // para separar los caracateres donde halla @. crea dos zonas 1 y 2
+                    String user_name = user.substring(0, pos);
+                    Toast.makeText(getApplicationContext(), "Bienbenido", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(getApplication(), dashboard.class);
+                    intent.putExtra("Usuario", user_name);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(getApplicationContext(), "Credenciar Incorrecta", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     private void register() {
         String user = usuario.getText().toString();
         String pass = contraseña.getText().toString();
 
-        mAuth.createUserWithEmailAndPassword(user,pass).addOnCompleteListener((OnCompleteListener<AuthResult>) this)
+        mAuth.createUserWithEmailAndPassword(user,pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Usuario Registrado", Toast.LENGTH_SHORT).show();
+                    clear();
+                    auth();
+                }else {
+                    if (task.getException() instanceof FirebaseAuthUserCollisionException){
+                        Toast.makeText(getApplicationContext(), "Este Usuario ya Existe", Toast.LENGTH_SHORT).show();
+                    }else {
+                        Toast.makeText(getApplicationContext(), "No se Creo el Usuario", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
     }
 
-//    private void validate() {
-//        String user = usuario.getText().toString();
-//        String pass = contraseña.getText().toString();
-//
-//        if (user.equals("")){
-//            usuario.setError("Requerrido");
-//        } else if (pass.equals("")){
-//            contraseña.setError("Requerrido");
-//        }
-//    }
+    private void clear () {
+        usuario.setText("");
+        contraseña.setText("");
+    }
+
+    private void validate() {
+        String user = usuario.getText().toString();
+        String pass = contraseña.getText().toString();
+
+        if (user.equals("")){
+            usuario.setError("Requerrido");
+        } else if (pass.equals("")){
+            contraseña.setError("Requerrido");
+        }
+    }
 }
--- app/src/main/java/com/example/organizador/dashboard.java
+++ app/src/main/java/com/example/organizador/dashboard.java
@@ -9,9 +9,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class dashboard extends AppCompatActivity {
+    private TextView titulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,9 @@ public class dashboard extends AppCompatActivity {
                 startActivityForResult(intent,0);
             }
         });
+        titulo = findViewById(R.id.titulo2);
+
+        titulo.setText("Bienvenido " + getIntent().getStringExtra("Usuario"));
 
         Bundle parametros = this.getIntent().getExtras();
         if(parametros !=null){
